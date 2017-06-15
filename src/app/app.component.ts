a2ed87eeea45190a4e39af6f1165c7899bda99b0
nazarpelekh <nazar_peleh@meta.ua>
2017-06-15 23:53:19 +0300

    Simply Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Inline Template</h1>
+  `,
+  styles: [`
+    h1 {
+      color: red;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'I change it!'
+  title = 'I change it!';
 }
