7a09ad487a669e662c98c5710f8f13ddabafc55c
nazarpelekh <nazar_peleh@meta.ua>
2017-06-20 22:44:50 +0300

    Binding

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -3,11 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-databinding',
   templateUrl: './databinding.component.html',
-  styleUrls: ['./databinding.component.css']
+  styleUrls: ['./databinding.component.css'],
 })
 export class DatabindingComponent  {
 
   stringInterpolation = 'This is string interpolation';
   numberInterpolation = 2;
+
+  onTest() {
+    return true;
+  }
+
+  onClicked(value: string) {
+    alert(value);
+  }
 
 }
