0c7e4b744ec6d5f02a601165b19972d84fbaee4d
nazarpelekh <nazar_peleh@meta.ua>
2017-06-22 23:32:32 +0300

    Hooks(OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy) and bindable value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,17 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>Root Component</h1>
-    <app-lifecycle *ngIf="!deletes"></app-lifecycle> 
+    <app-lifecycle *ngIf="!delete" [bindable]="boundValue"></app-lifecycle> 
+    <p>{{test}}</p>
     <button (click)="delete = true">Click To Delete</button>
+    <button (click)="test = 'Changed Value'">Click To Change Content</button>
+    <button (click)="boundValue = 2000">Click To Change Binding</button>
   `,
 })
 export class AppComponent {
   title = 'I change it!';
   delete = false;
+  test = 'Starting Value';
+  boundValue = 1000;
 
 }
