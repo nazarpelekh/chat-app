65170ecc6010caad3d84a982c1bd8d3e13ffbb80
nazarpelekh <nazar_peleh@meta.ua>
2017-07-06 15:07:01 +0300

    directives&binding

--- 02_Components/01_Components/app/app.module.js
+++ 02_Components/01_Components/app/app.module.js
@@ -5,9 +5,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
+Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var router_1 = require("@angular/router"); // модуль для маршрутизации
@@ -17,22 +15,21 @@ var list2_component_1 = require("./list2/list2.component");
 var AppModule = (function () {
     function AppModule() {
     }
-    AppModule = __decorate([
-        core_1.NgModule({
-            imports: [
-                platform_browser_1.BrowserModule,
-                router_1.RouterModule.forRoot([
-                    { path: "list1", component: list1_component_1.List1Component },
-                    { path: "list2", component: list2_component_1.List2Component },
-                    { path: "", redirectTo: "list1", pathMatch: "full" }
-                ])
-            ],
-            declarations: [app_component_1.AppComponent, list1_component_1.List1Component, list2_component_1.List2Component],
-            bootstrap: [app_component_1.AppComponent]
-        }), 
-        __metadata('design:paramtypes', [])
-    ], AppModule);
     return AppModule;
 }());
+AppModule = __decorate([
+    core_1.NgModule({
+        imports: [
+            platform_browser_1.BrowserModule,
+            router_1.RouterModule.forRoot([
+                { path: "list1", component: list1_component_1.List1Component },
+                { path: "list2", component: list2_component_1.List2Component },
+                { path: "", redirectTo: "list1", pathMatch: "full" }
+            ])
+        ],
+        declarations: [app_component_1.AppComponent, list1_component_1.List1Component, list2_component_1.List2Component],
+        bootstrap: [app_component_1.AppComponent]
+    })
+], AppModule);
 exports.AppModule = AppModule;
 //# sourceMappingURL=app.module.js.map
--- 02_Components/02_BarrelFiles/app/app.module.js
+++ 02_Components/02_BarrelFiles/app/app.module.js
@@ -5,9 +5,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
+Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var router_1 = require("@angular/router"); // модуль для маршрутизации
@@ -16,20 +14,18 @@ var index_1 = require("./lists/index"); // использование barrel fil
 var AppModule = (function () {
     function AppModule() {
     }
-    AppModule = __decorate([
-        // использование barrel file
-        core_1.NgModule({
-            imports: [
-                platform_browser_1.BrowserModule,
-                index_1.ListsModule,
-                router_1.RouterModule.forRoot(index_1.routs)
-            ],
-            declarations: [app_component_1.AppComponent],
-            bootstrap: [app_component_1.AppComponent]
-        }), 
-        __metadata('design:paramtypes', [])
-    ], AppModule);
     return AppModule;
 }());
+AppModule = __decorate([
+    core_1.NgModule({
+        imports: [
+            platform_browser_1.BrowserModule,
+            index_1.ListsModule,
+            router_1.RouterModule.forRoot(index_1.routs)
+        ],
+        declarations: [app_component_1.AppComponent],
+        bootstrap: [app_component_1.AppComponent]
+    })
+], AppModule);
 exports.AppModule = AppModule;
 //# sourceMappingURL=app.module.js.map
